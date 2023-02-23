b8ef4a6249cbf1f76fc7236302f629e9371b9ff4
Rapha Lobosco <raphacomics@gmail.com>
2023-02-23 10:22:24 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import json from '../catalog.json'
+import { RouterLink, RouterView } from "vue-router";
+import json from "../catalog.json";
 
-const favAlbums = json
-
+const favAlbums = json;
 </script>
 
 <template>
   <main class="container w-50">
-    <header class="d-flex py-3 border-bottom mb-4 align-items-center justify-content-between">
+    <header
+      class="d-flex py-3 border-bottom mb-4 align-items-center justify-content-between"
+    >
       <div class="col">
         <h1>Favourite Albums</h1>
       </div>
       <div class="col-auto">
-        <button class="btn btn-primary">Add new album</button>
+        <button class="btn btn-primary">+ new album</button>
       </div>
     </header>
 
-    <div v-for="item in favAlbums" class="d-flex align-items-center p-3 border rounded-3 shadow-sm">
+    <div
+      v-for="item in favAlbums"
+      class="d-flex align-items-center p-3 border rounded-3 shadow-sm"
+    >
       <div class="me-3">
-        <img class="img-fluid rounded-3" :src="item.image" alt="">
+        <img class="img-fluid rounded-3" :src="item.image" alt="" />
       </div>
       <div class="d-flex align-items-center justify-content-between w-100">
         <div>
@@ -28,17 +32,12 @@ const favAlbums = json
           <p class="badge m-0 text-bg-secondary">{{ item.genre }}</p>
         </div>
 
-
-
         <div>
           <span class="text-secondary me-4">Remove</span>
-          <span class="text-primary ">Edit</span>
+          <span class="text-primary">Edit</span>
         </div>
-
       </div>
-
     </div>
-
   </main>
   <RouterView />
 </template>
