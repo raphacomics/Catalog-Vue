f6d4ae3a9657612ad3c69e92bb28ab459494e9ab
Rapha Lobosco <83170908+raphacomics@users.noreply.github.com>
2023-03-16 20:34:26 +0000

    clean up code

--- src/components/AlbumsList.vue
+++ src/components/AlbumsList.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import { uid } from 'uid'
 import Modal from "../components/Modal.vue"
 import albumData from "../catalog.json";
-import newAlbum from "./newAlbum.vue"
+import Album from "./Album.vue"
 
 const albums = albumData;
 
@@ -36,28 +36,9 @@ const addAlbum = (newAlbum) => {
     </header>
 
     <!-- NEW LIST -->
-    <newAlbum v-for="album in albumNew" :album="album" />
+    <Album v-for="album in albumNew" :album="album" />
     <!-- LIST -->
-    <div v-for="(album, index) in albums" class="d-flex align-items-center p-3 border rounded-3 shadow-sm mb-3"
-        :key="index">
-        <div class="me-3">
-            <img class="img-fluid rounded-3" :src="album.image" alt="" />
-        </div>
-        <div class="d-flex justify-content-between w-100">
-            <div>
-                <h5 class="mb-0">{{ album.name }}</h5>
-                <p class="mb-1">{{ album.artist }}</p>
-                <p class="badge m-0 text-bg-light border rounded-pill border-dark-subtle">
-                    {{ album.genre }}
-                </p>
-            </div>
-
-            <div class="d-flex gap-2">
-                <div class="col-auto"><v-icon class="pointer" name="hi-pencil" fill="#0096FF" /><small>edit</small></div>
-                <div class="col-auto"><v-icon class="pointer" name="hi-trash" fill="red" /><small>remove</small></div>
-            </div>
-        </div>
-    </div>
+    <Album v-for="album in albums" :album="album" />
 </template>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello!</h1>
   </main>
 </template>
